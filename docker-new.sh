cc291fcb87e8c00cb640cb2ad8a24e18be391340
onezero1010101 <onezero1010101@gmail.com>
2020-08-04 07:26:40 -0400

    Create docker-new.sh

---
+++ docker-new.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+sudo usermod -aG docker $USER
+
+ver=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+sudo curl -L "https://github.com/docker/compose/releases/download/$ver/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
