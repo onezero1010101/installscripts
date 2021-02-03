c71eeff866b0f5a61d91bd9a9970b97d55e9c23f
onezero1010101 <onezero1010101@gmail.com>
2021-02-03 07:42:24 -0500

    Update docker-new.sh

--- docker-new.sh
+++ docker-new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -fsSL https://get.docker.com -o get-docker.sh | bash 
-#sh get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh 
+sh get-docker.sh
 
 sudo usermod -aG docker $USER
 
